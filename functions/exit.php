60ed289655e0f6c5fa3501b7071832978302ab97
Kostya <ko.klochkov@gmail.com>
2021-08-04 16:23:22 +0300

    cookie

--- functions/exit.php
+++ functions/exit.php
@@ -1,4 +1,5 @@
 <?php
+setcookie('user',$user['id'],time() - 3600, "/");
 setcookie('user',$user['fio'],time() - 3600, "/");
 header("Location: /")
 ?>
--- functions/login.php
+++ functions/login.php
@@ -2,16 +2,10 @@
 $login = $_POST["login"];
 $pass = $_POST["pass"];
 require '../config.php';
-$mysql = new mysqli('localhost', 'root', 'root', 'e-test');
 $sql = ("SELECT * FROM `teachers` WHERE `login` = '$login' AND `pass` = '$pass'");
 $rez = mysqli_query($link, $sql);
 $user = $rez->fetch_assoc(); 
-// if(count($user) == 0){
-//     echo "<h3 style='color:red; text-align:center;'>Ошибка Нет такого пользователя!</h3>";
-//     exit();
-// }
 setcookie('user',$user['id'],time() + 3600, "/");
 setcookie('fio',$user['fio'],time() + 3600, "/");    
-$mysql->close();
 header("Location: /forlogin.php");
 ?>
